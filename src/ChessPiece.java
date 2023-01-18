adb75fafe3e6f04991e6f20fc0752045386c52ff
Sergey_mm <C.A.Malofeev@gmail.com>
2023-01-18 15:08:05 +0300

    commit 2

--- src/ChessPiece.java
+++ src/ChessPiece.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class ChessPiece {
+abstract public class ChessPiece {
+    String color;
+    Boolean chek = true;
+
+    public ChessPiece(String color) {
+        this.color = color;
+    }
+    public String getColor(){
+        return color;
+    }
+    public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn){
+        return chek.booleanValue();
+
+    }
 }
